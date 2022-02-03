435a0241c921dbe4d7161fe0afac2bae2019ffc2
Chris Leibl <christopher@MacBook-Pro.local>
2022-02-03 13:36:40 -0500

    update outputs

--- 2-environments/modules/env_baseline/outputs.tf
+++ 2-environments/modules/env_baseline/outputs.tf
@@ -26,12 +26,12 @@ output "monitoring_project_id" {
 
 output "base_shared_vpc_project_id" {
   description = "Project for base shared VPC network."
-  value       = length(module.base_shared_vpc_host_project) > 0 ? module.base_shared_vpc_host_project.project_id : null
+  value       = length(module.base_shared_vpc_host_project) > 0 ? module.base_shared_vpc_host_project[0].project_id : null
 }
 
 output "restricted_shared_vpc_project_id" {
   description = "Project for restricted shared VPC network."
-  value       = length(module.restricted_shared_vpc_host_project) > 0 ? module.restricted_shared_vpc_host_project.project_id : null
+  value       = length(module.restricted_shared_vpc_host_project) > 0 ? module.restricted_shared_vpc_host_project[0].project_id : null
 }
 
 output "env_secrets_project_id" {
